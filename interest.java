40c4393465d7b66099f87e832c5c939bbdf25d62
Aditya Goswami <97277517+Adityagoswamii@users.noreply.github.com>
2022-08-24 14:56:27 +0530

    Add files via upload

---
+++ interest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ class Simple
+ {
+   public static void main(String[]args)
+    {
+       int p=20000;
+       int r=5;
+       int t=3;
+       float i;
+       i=p*r*t/100;
+       System.out.print("The simple interest is" +i);
+    }
+}
+
+
